24a3ebb4dacb576a1d282230afe9edf43fa0fa63
Vincent Delaitre <vincent.delaitre@gmail.com>
2016-10-17 14:22:17 +0200

    intial commit

---
+++ deepomatic/deepomatic.py
@@ -0,0 +1,399 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2014 Deepomatic SAS
+http://www.deepomatic.com/
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import requests
+import json
+import base64
+
+###############################################################################
+
+API_VERSION = 0.6
+API_HOST = 'https://api.deepomatic.com'
+
+###############################################################################
+
+class Point(object):
+    def __init__(self, x, y):
+        self.point = {'x' : x, 'y' : y}
+    def __getitem__(self):
+        return self.point
+
+class Polygon(object):
+    def __init__(self):
+        self.points = []
+
+    def addPoint(self, point):
+        self.points.append(point.point)
+
+    def addPoints(self, points):
+        for pt in points:
+            self.points.append(pt.point)
+
+    def __getitem__(self):
+        return self.points
+
+
+class Bbox(object):
+    def __init__(self, min, max):
+        self.corners = {'xmin' : min.point['x'], 'ymin' : min.point['y'], 'xmax' : max.point['x'], 'ymax' : max.point['y']}
+
+    def __getitem__(self):
+        return self.corners
+
+class ImgsSend(object):
+    def __init__(self, sourceType, source, polygon = None, bbox = None):
+        self.imgs = []
+        if sourceType == "file":
+            with open(source, "rb") as image_file:
+                source = base64.b64encode(image_file.read())
+            sourceType = "base64"
+        self.imgs.append({sourceType : source})
+        if polygon is not None:
+            self.imgs[0].update({"polygon" : polygon})
+        if bbox is not None:
+            self.imgs[0].update({"bbox" : bbox})
+
+    def addObject(self, sourceType, source, polygon = None, bbox = None):
+        last = len(self.imgs)
+        if sourceType == "file":
+            with open(source, "rb") as image_file:
+                source = base64.b64encode(image_file.read())
+            sourceType = "base64"
+        self.imgs.append({sourceType : source})
+        if polygon is not None:
+            self.imgs[last].update(polygon)
+        if bbox is not None:
+            self.imgs[last].update(bbox)
+
+    def __getitem__(self):
+        return self.imgs
+
+class BatchObject:
+    def __init__(self, dbname):
+        self.db = dbname
+        self.requests = []
+
+    def deleteObject(self, id):
+        self.requests.append({"db" : self.db, "id" : id, "method" : "DELETE"})
+
+    def getObject(self, id):
+        self.requests.append({"db" : self.db, "id" : id, "method" : "GET"})
+
+    def addObject(self, imgs, id = None):
+        if id != None:
+            self.requests.append({"db" : self.db, "imgs" : imgs.imgs, "method" : "POST"})
+        else :
+            self.requests.append({"db" : self.db, "id" : id, "imgs" : imgs.imgs, "method" : "PUT"})
+
+    def __getitem__(self):
+        return self.requests
+
+
+
+class HTTPHelper(object):
+
+    def __init__(self, app_id, api_key, verify, host):
+        """
+        Init the HTTP helper with API key and secret
+        """
+        self.api_key = str(api_key)
+        self.app_id = str(app_id)
+
+        self.verify = verify
+        if not host is None:
+            self.host = host
+        if self.host[-1] == '/':
+            self.host = self.host[:-1]
+
+    #--------------------------------------------------------------------
+
+    def setupHeaders(self, headers = None):
+        """
+        Build authentification header
+        """
+        if headers is None:
+            headers = dict([])
+
+        if not 'Accept' in headers:
+            headers['Accept'] = 'application/json'
+        headers['X-APP-ID'] = self.app_id
+        headers['X-API-KEY'] = self.api_key
+
+        return headers
+
+    #--------------------------------------------------------------------
+
+    def response(self, r):
+        """
+        Decode the response returned by the server
+        """
+        if r.status_code == 204: #delete
+            return "No Content", r.status_code
+
+        if 'application/json' in r.headers['Content-Type']:
+            return r.json(), r.status_code
+        else:
+            return r.content, r.status_code
+
+    #--------------------------------------------------------------------
+
+    def formatParams(self, data):
+        if data:
+            for key, value in data.items():
+                if isinstance(value, bool):
+                    data[key] = int(value)
+                elif isinstance(value, dict):
+                    data[key] = json.dumps(value)
+        return data
+
+    #--------------------------------------------------------------------
+
+    def helperGD(self, func, resource, params):
+        r = func(self.host + resource,
+                    headers = self.setupHeaders(),
+                    params  = self.formatParams(params),
+                    verify  = self.verify)
+        return self.response(r)
+
+    #--------------------------------------------------------------------
+
+    def helperPPP(self, func, resource, params, data, contentType, files):
+        if contentType.startswith('application/json') and not isinstance(data, basestring):
+            data = json.dumps(data)
+
+        r = func(self.host + resource,
+                    params = self.formatParams(params),
+                    data = data,
+                    files = files,
+                    headers = self.setupHeaders({"Content-Type": contentType}),
+                    verify = self.verify)
+        return self.response(r)
+
+    #--------------------------------------------------------------------
+
+    def get(self, resource, params = None):
+        """
+        Perform a GET request
+        """
+        return self.helperGD(requests.get, resource, params)
+
+    #--------------------------------------------------------------------
+
+    def delete(self, resource, params = None):
+        """
+        Perform a DELETE request
+        """
+        return self.helperGD(requests.delete, resource, params)
+
+    #--------------------------------------------------------------------
+
+    def put(self, resource, params = None, data = None, contentType = 'application/json', files = None):
+        """
+        Perform a PUT request
+        """
+        return self.helperPPP(requests.put, resource, params, data, contentType, files)
+
+    #--------------------------------------------------------------------
+
+    def post(self, resource, params = None, data = None, contentType = 'application/json', files = None):
+        """
+        Perform a POST request
+        """
+        return self.helperPPP(requests.post, resource, params, data, contentType, files)
+
+    #--------------------------------------------------------------------
+
+    def patch(self, resource, params = None, data = None, contentType = 'application/json', files = None):
+        """
+        Perform a PATCH request
+        """
+        return self.helperPPP(requests.patch, resource, params, data, contentType, files)
+
+
+###############################################################################
+
+class Client(object):
+
+    def __init__(self, app_id, api_key, verify = False, host = API_HOST, version = API_VERSION):
+        if app_id == None or api_key == None:
+            raise Exception("Please specify APP_ID and API_KEY.")
+
+        # Until v1.0, we force explicit versioning
+        if version is None:
+            raise Exception("API version should be explicitly mentionned.")
+
+        if version is not None:
+            if not isinstance(version, basestring):
+                version = 'v%g' % version
+            elif version[0] != 'v':
+                version = 'v' + version
+
+            host += '/' + version
+
+        self.helper = HTTPHelper(app_id, api_key, verify, host)
+
+    #--------------------------------------------------------------------
+
+    def _response_(self, response, status, okStatus = [200], getStatus = False):
+        if status in okStatus:
+            return response if getStatus is False else (response, status)
+        else:
+            if isinstance(response, basestring):
+                raise Exception(response)
+            elif "message" in response:
+                raise Exception(response['message'])
+            else:
+                raise Exception(str(response))
+
+    #--------------------------------------------------------------------
+
+    def waitForCompletion(self, response) :
+        while self.retrieveTask(response["task_id"])['task']['status'] == "pending" :
+            continue
+
+    #--------------------------------------------------------------------
+
+    def getDBs(self):
+        response, status = self.helper.get('/search/dbs')
+        return self._response_(response, status)
+
+    #--------------------------------------------------------------------
+
+    def clearDB(self, db, wait = False):
+        response, status = self.helper.delete('/search/dbs/%s/objects' % db)
+        complete_response = self._response_(response, status, [200])
+        if wait :
+            self.waitForCompletion(complete_response)
+        return complete_response
+
+    #--------------------------------------------------------------------
+
+    def deleteDB(self, db, wait = False):
+        response, status = self.helper.delete('/search/dbs/%s' % db)
+        complete_response = self._response_(response, status, [200])
+        if wait :
+            self.waitForCompletion(complete_response)
+        return complete_response
+
+    #--------------------------------------------------------------------
+
+    def getCount(self, db):
+        response, status = self.helper.get('/search/dbs/%s/count/' % db)
+        return self._response_(response, status, [200])
+
+    #--------------------------------------------------------------------
+
+    def saveObject(self, db, id = None, imgs = [], data = {}):
+        for img in imgs:
+            if "file" in img:
+                with open(img["file"], "rb") as image_file:
+                    img = base64.b64encode(image_file.read())
+                img += {"base64" : img}
+                del img["file"]
+
+        obj = {
+            'imgs' : imgs,
+            'data' : data,
+        }
+        if id is None:
+            response, status = self.helper.post('/search/dbs/%s/objects' % db, data = obj)
+        else:
+            response, status = self.helper.put('/search/dbs/%s/objects/%s' % (db, str(id)), data = obj)
+        return self._response_(response, status)
+
+    #--------------------------------------------------------------------
+
+    def retrieveTask(self, task_id):
+        response, status = self.helper.get('/tasks/%s/' % task_id)
+        return self._response_(response, status)
+
+    #--------------------------------------------------------------------
+
+    def getObject(self, db, id):
+        response, status = self.helper.get('/search/dbs/%s/objects/%s' % (db, str(id)))
+        return self._response_(response, status)
+
+    #--------------------------------------------------------------------
+
+    def deleteObject(self, db, id, wait = False):
+        response, status = self.helper.delete('/search/dbs/%s/objects/%s' % (db, str(id)))
+        complete_response = self._response_(response, status)
+        if wait :
+            self.waitForCompletion(complete_response)
+        return complete_response
+
+    #--------------------------------------------------------------------
+
+    def search(self, db, source, wait = False, **kwargs):
+        """
+        Search for similar images.
+
+        Take:
+        - db      : the database name.
+        - source  : the url or file.
+        - filters : filters under the form of a MongoDB request. The query is
+                      performed on the data field of the database object.
+        - skip    : number of results to skip. Defaults to 0.
+        - limit   : number of results to return. Defaults to 100.
+
+        Give:
+        - hits    : list of matching images.
+        """
+
+        if "file" in source:
+            with open(source["file"], "rb") as image_file:
+                img = base64.b64encode(image_file.read())
+            source = {"base64" : img}
+
+        params = source
+
+        for k, v in kwargs.items():
+            params[k] = v
+
+        response, status = self.helper.get('/search/query/%s/' % db, params = params)
+        complete_response = self._response_(response, status)
+        if wait :
+            self.waitForCompletion(complete_response)
+            return self._response_(self.retrieveTask(response["task_id"])['task']['data'], status)
+        return complete_response
+
+    #--------------------------------------------------------------------
+    # Send Batch request
+    def batchRequest(self, requests):
+        data = {
+            "requests" : requests.requests
+        }
+        response, status = self.helper.post('/search/batch/dbs', data = json.dumps(data))
+        return self._response_(response, status)
+
+
+    def detect(self, detector_type, img_url, wait = False) :
+        response, status = self.helper.get('/detect/%s/url/?query=%s' % (detector_type, img_url))
+        complete_response = self._response_(response, status)
+        if wait :
+            self.waitForCompletion(complete_response)
+        return complete_response
+
+
+###############################################################################
---
+++ demo.py
@@ -0,0 +1,164 @@
+import os
+import time
+import json
+from deepomatic import deepomatic
+
+appID  = os.environ['DEEPOMATIC_APP_ID']
+apiKey = os.environ['DEEPOMATIC_API_KEY']
+
+client = deepomatic.Client(appID, apiKey)
+
+# ---------------------------------------------------------------------------------------------------
+# For the sake of reproducibility, we always wait for the tasks to complete via a check on the taskID
+# ---------------------------------------------------------------------------------------------------
+
+def db_and_indexing(db_name_1, db_name_2) :
+
+	print("--------------------------------------------------------------------------------------------")
+	print("--------- DBs and indexing")
+	print("--------------------------------------------------------------------------------------------")
+
+	# Delete the dbs if they exists for reproductibily
+	print("DELETE db %s if it exists." % db_name_1)
+	client.deleteDB(db_name_1, wait = True)
+
+	print("DELETE db %s if it exists." % db_name_2)
+	client.deleteDB(db_name_2, wait = True)
+
+
+	# POST an image into the db. An id is automatically created and returned.
+	# A task id is also returned to track the completion of the task
+	print("POST a new entry in %s with an auto-assigned id." % db_name_1)
+	img = [{"url": "https://s3-eu-west-1.amazonaws.com/deepo-public/Demo/shoes/shoes_0.jpg"}]
+	client.saveObject(db_name_1, imgs = img)
+
+	# PUT an image into the db with a defined id.
+	# A task id is returned to track the completion of the task
+	print("PUT a new entry in %s with a predefined id." % db_name_1)
+	img = [{"url": "https://s3-eu-west-1.amazonaws.com/deepo-public/Demo/shoes/shoes_1.jpg"}]
+	predefined_id = "shoes_1"
+	client.saveObject(db_name_1, id = predefined_id, imgs = img)
+
+	print("POST a new entry in %s with an auto-assigned id." % db_name_2)
+
+	bbox = deepomatic.Bbox(deepomatic.Point(0,0), deepomatic.Point(1,1))
+	poly = deepomatic.Polygon()
+	poly.addPoints([deepomatic.Point(0,0), deepomatic.Point(1,1), deepomatic.Point(0,1), deepomatic.Point(1,0)])
+	obj = deepomatic.ImgsSend("url", "https://s3-eu-west-1.amazonaws.com/deepo-public/Demo/shoes/shoes_2.jpg", bbox = bbox.corners, polygon = poly.points)
+	client.saveObject(db_name_2, imgs = obj.imgs)
+
+	print("DBs are %s." % ", ".join(client.getDBs()['dbs']))
+
+	print("CLEAR db %s." % db_name_2)
+	client.clearDB(db_name_2, wait = True)
+
+	print("DBs are now %s." % ", ".join(client.getDBs()["dbs"]))
+
+	print("DELETE db %s." % db_name_2)
+	delete_task_3 = client.deleteDB(db_name_2, wait = True)
+
+	print("DBs are now just %s." % ", ".join(client.getDBs()["dbs"]))
+
+	print("DB %s has %s element." % (db_name_1, str(client.getCount(db_name_1)["count"])))
+
+	print("GET object with id %s in db %s." % (predefined_id, db_name_1))
+	get_result_box = client.getObject(db_name_1, predefined_id)['object']['imgs'][0]['bbox']
+	print("Indexation has automatically cropped background color to {xmin: %.3f, ymin: %.3f, xmax: %.3f, ymax: %.3f}" % (get_result_box['xmin'], get_result_box['ymin'], get_result_box['xmax'], get_result_box['ymax']))
+
+	print("DELETE object with id %s in db %s." % (predefined_id, db_name_1))
+	object_delete = client.deleteObject(db_name_1, predefined_id, wait = True)
+
+	print("DB %s has now %s element." % (db_name_1, str(client.getCount(db_name_1)["count"])))
+
+# ---------------------------------------------------------------
+
+def indexing_in_batch(db_name) :
+
+	print("--------------------------------------------------------------------------------------------")
+	print("--------- Indexing in batch")
+	print("--------------------------------------------------------------------------------------------")
+
+	print("DELETE db %s if it exists." % db_name)
+	client.deleteDB(db_name, wait = True)
+
+	img = [{"url": "https://s3-eu-west-1.amazonaws.com/deepo-public/Demo/shoes/shoes_1.jpg"}]
+	predefined_id = "batchTestClient"
+	client.saveObject(db_name, id = predefined_id, imgs = img)
+
+	batch = deepomatic.BatchObject(db_name)
+
+	print("PUT a batch of 20 images in %s with predefined ids." % db_name)
+	url_base = "https://s3-eu-west-1.amazonaws.com/deepo-public/Demo/shoes/shoes_%s.jpg"
+	for i in range(0,20) :
+		obj = deepomatic.ImgsSend("url", url_base % i)
+		batch.addObject(obj, "shoes_%d" % i)
+
+	batch_response = client.batchRequest(batch)
+
+
+	print("DB %s has now %s element." % (db_name, str(client.getCount(db_name)['count'])))
+
+
+# ---------------------------------------------------------------
+
+def searching(db_name) :
+
+	if not db_name in client.getDBs()["dbs"] :
+		indexing_in_batch()
+
+	print("--------------------------------------------------------------------------------------------")
+	print("--------- searching")
+	print("--------------------------------------------------------------------------------------------")
+
+	img = [{"url": "https://s3-eu-west-1.amazonaws.com/deepo-public/Demo/shoes/shoes_0.jpg"}]
+	client.saveObject(db_name, imgs = img)
+	image_test_url = {"url" : "https://s3-eu-west-1.amazonaws.com/deepo-public/Demo/shoes/shoes_20.jpg"}
+	search_results = client.search(db_name, image_test_url, wait = True)
+	print("The id of the image that is the most similar to the query is %s with a score of %.5f" % (search_results['hits'][0]['id'], search_results['hits'][0]['score']))
+
+def perfect_match(db_name) :
+
+	print("--------------------------------------------------------------------------------------------")
+	print("--------- Perfect Match")
+	print("--------------------------------------------------------------------------------------------")
+
+	print("DELETE db %s if it exists." % db_name)
+	client.deleteDB(db_name, wait = True)
+
+	url_base = "https://s3-eu-west-1.amazonaws.com/deepo-public/Demo/perfect_match/%s.jpg"
+	poster_names = ['elle', 'jurassic_shark', 'le_chasseur', 'sos_fantome', 'the_dark_night']
+	print("PUT 5 movie posters in db %s." % db_name)
+
+	batch = deepomatic.BatchObject(db_name)
+	for name in poster_names :
+		obj = deepomatic.ImgsSend("url", url_base % name)
+		batch.addObject(obj, name)
+
+	batch_response = client.batchRequest(batch)
+
+	time.sleep(5)
+
+	image_test = "https://s3-eu-west-1.amazonaws.com/deepo-public/Demo/perfect_match/query.jpg"
+	search_results = client.search(db_name, {'url' : image_test}, wait = True, perfect_match = True)
+	print("Did we found a perfect match: %s" % str(search_results['hits'][0]['is_perfect_match']))
+
+def detection() :
+
+	print("--------------------------------------------------------------------------------------------")
+	print("--------- detection")
+	print("--------------------------------------------------------------------------------------------")
+
+	response = client.detect("fashion", "http://static1.puretrend.com/articles/4/12/06/94/@/1392954-kim-kardashian-dans-les-rues-de-los-580x0-3.jpg", wait = True)
+
+	results_detection = client.retrieveTask(response["task_id"])
+
+	print("Here is what we have deteted:")
+	print(json.dumps(results_detection['task']['data']['boxes'], indent=2, sort_keys=True))
+
+
+db_and_indexing("demo_1", "demo_2")
+indexing_in_batch("batch_indexing")
+searching("batch_indexing")
+perfect_match("perfect_match")
+detection()
+
