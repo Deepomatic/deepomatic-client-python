6beccbe91c4baf86475041a5ec3bb5bb1ad21a58
Vincent Delaitre <vincent@deepomatic.com>
2018-03-09 12:40:39 +0100

    better version with virtualenv

--- tests/install_python2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "N" | apt-get --no-install-recommends -y install openssh-client
-apt-get --no-install-recommends -y install python-setuptools python-dev libffi-dev libssl-dev curl g++
-curl https://bootstrap.pypa.io/get-pip.py | python
-pip install --upgrade pip
-
-# Install requirements
-pip install -r requirements.txt
