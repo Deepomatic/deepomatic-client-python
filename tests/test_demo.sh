3773fc081d99e626348600b78ab584adc6e77b1a
Vincent Delaitre <vincent@deepomatic.com>
2018-03-09 14:06:24 +0100

    remove python 3.5

--- tests/test_demo.sh
+++ tests/test_demo.sh
@@ -5,7 +5,7 @@ set -e
 # Copy the demo outside of the package
 cp demo.py /tmp/demo.py
 
-for version in '2.7' '3.5' '3.6'; do
+for version in '2.7' '3.6'; do
     echo "Testing Python ${version}"
     virtualenv -p python${version} venv_${version}
     bash -c "source venv_${version}/bin/activate && pip install . && python /tmp/demo.py"
