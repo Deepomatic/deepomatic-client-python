aed6b73ff4954f40482a8c38f2bbd4dddb3a4b9c
Vincent Delaitre <vincent@deepomatic.com>
2018-03-09 13:09:41 +0100

    use python 3.6

--- tests/test_demo.sh
+++ tests/test_demo.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for version in '2.7' '3'; do
+for version in '2.7' '3.6'; do
     echo "Testing Python ${version}"
     virtualenv -p python${version} venv_${version}
     bash -c "source venv_${version}/bin/activate && pip install -r requirements.txt && python demo.py"
