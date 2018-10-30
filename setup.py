771bedeab4032d757d8584feeda06c56698839e2
Hugo <hugo@deepomatic.com>
2018-10-30 16:44:32 +0100

    add python versions restrictions

--- setup.py
+++ setup.py
@@ -36,10 +36,16 @@ setup(
     include_package_data=True,
     long_description=README,
     install_requires=[str(ir.req) for ir in install_reqs],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     classifiers=[
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ]
 )
