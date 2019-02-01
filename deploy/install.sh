e9afd121eae8eee58992616e0f8d626c267482d6
Hugo Maingonnat <hugo@deepomatic.com>
2019-02-01 16:15:24 +0100

    coverage

--- deploy/install.sh
+++ deploy/install.sh
@@ -4,4 +4,4 @@ set -e
 
 apt-get update && apt-get install -y build-essential
 pip install -r requirements.txt
-pip install pytest==4.0.2 # for testing
+pip install pytest==4.0.2 pytest-cov==2.6.1 pytest-voluptuous==1.1.0 # for testing
