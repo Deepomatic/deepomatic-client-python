48b4ce4a1a88094fb781f7195f3015f82876347c
Hugo Maingonnat <hugo@deepomatic.com>
2019-05-27 14:07:29 +0200

    bump version

--- deepomatic/api/version.py
+++ deepomatic/api/version.py
@@ -1,6 +1,6 @@
 __title__ = 'deepomatic-api'
 __description__ = 'Deepomatic API client'
-__version__ = '0.8.1'
+__version__ = '0.8.2'
 __author__ = 'deepomatic'
 __author_email__ = 'support@deepomatic.com'
 __url__ = 'https://github.com/deepomatic/deepomatic-client-python'
