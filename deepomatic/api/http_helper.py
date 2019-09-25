ce8504380b3250580c7f4ed9074d565c23bade48
Hugo Maingonnat <hugo@deepomatic.com>
2019-09-25 11:55:03 +0200

    remove support for user_agent_suffix

--- deepomatic/api/http_helper.py
+++ deepomatic/api/http_helper.py
@@ -50,7 +50,7 @@ class RequestsTimeout(object):
 class HTTPHelper(object):
     def __init__(self, app_id=None, api_key=None, verify_ssl=None,
                  host=None, version=API_VERSION, check_query_parameters=True,
-                 user_agent_prefix='', user_agent_suffix='', pool_maxsize=20,
+                 user_agent_prefix='', pool_maxsize=20,
                  requests_timeout=RequestsTimeout.FAST, **kwargs):
         """
         Init the HTTP helper with API key and secret.
@@ -102,9 +102,6 @@ class HTTPHelper(object):
 
         self.user_agent += '{package_title}-python-client/{package_version} requests/{requests_version} python/{python_version} platform/{platform}\
             '.format(**user_agent_params)
-
-        if user_agent_suffix:
-            self.user_agent += ' ' + user_agent_suffix
 
         self.api_key = str(api_key)
         self.app_id = str(app_id)
