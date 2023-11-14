1909d0e941bad0ac92825f234e5acb20d4fca4f2
Hugo Maingonnat <hugo@deepomatic.com>
2023-11-14 11:00:11 +0100

    Improve retry error

--- tests/test_client.py
+++ tests/test_client.py
@@ -348,6 +348,29 @@ class TestClientRetry(object):
         assert isinstance(exc, ConnectionError)
         assert 'Name or service not known' in str(exc)
 
+        spec = client.RecognitionSpec.retrieve('imagenet-inception-v3')  # doesn't make any http call
+        with pytest.raises(HTTPRetryError) as exc:
+            print(spec.data())  # does make a http call
+
+        assert str(exc.value).startswith("""
+        Last attempt was an exception <class 'requests.exceptions.ConnectionError'> "HTTPConnectionPool(host='invalid-domain.deepomatic.com', port=80): Max retries exceeded with url: /v0.7/recognition/public/imagenet-inception-v3/ (Caused by NewConnectionError('<urllib3.connection.HTTPConnection object at
+        """.strip())
+        assert str(exc.value).endswith("""
+        >: Failed to establish a new connection: [Errno -2] Name or service not known',))"
+        """.strip())
+
+    def test_retry_bad_status_code(self):
+        client = get_client(host='https://httpbin.org', version=None)
+        with httpretty.enabled():
+            httpretty.register_uri(
+                httpretty.GET,
+                re.compile(r'https?://.*?/?'),
+                status=502,
+            )  # to avoid flakyness we prefer to mock even though httpbin is done for that
+            with pytest.raises(HTTPRetryError) as retry_error:
+                client.http_helper.get('/status/502')
+            assert str(retry_error.value) == "Last attempt was a Response <status_code=502 method=GET url=https://httpbin.org//status/502>"
+
     def register_uri(self, methods, status):
         for method in methods:
             httpretty.register_uri(
@@ -410,6 +433,7 @@ class TestClientRetry(object):
             last_exception = retry_error.last_attempt.exception(timeout=0)
             assert isinstance(last_exception, HTTPRetryError)
             assert 502 == last_exception.last_attempt.result().status_code
+            assert str(last_exception)
 
     def test_no_retry_blacklist_exception(self):
         client = self.get_client_with_retry()
